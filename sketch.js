4ffd7303d133ec99ac4f5d2901744e90bb5fb5d2
ksaouk <70230383+ksaouk@users.noreply.github.com>
2020-09-01 12:58:06 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,20 +2,22 @@ var movingRect, fixedRect;
 
 function setup() {
   createCanvas(1200,800);
-  fixedRect = createSprite(400, 200, 50, 80);
+  fixedRect = createSprite(400, 100, 50, 80);
   fixedRect.shapeColor = "blue";
+  fixedRect.velocityY = 4;
 
-  movingRect = createSprite(400, 200, 80, 50);
+  movingRect = createSprite(400, 800, 80, 50);
   movingRect.shapeColor = "blue";
+  movingRect.velocityY = -4;
 }
 
 function draw() {
   background(0,0,0);  
   
-  movingRect.x = World.mouseX;
-  movingRect.y = World.mouseY;
+  //movingRect.x = World.mouseX;
+  //movingRect.y = World.mouseY;
 
-  if(movingRect.x - fixedRect.x < movingRect.width/2+fixedRect.width/2 && 
+  /*if(movingRect.x - fixedRect.x < movingRect.width/2+fixedRect.width/2 && 
     fixedRect.x - movingRect.x < movingRect.width/2+fixedRect.width/2 && 
     movingRect.y - fixedRect.y < movingRect.height/2+fixedRect.height/2 && 
     fixedRect.y - movingRect.y < movingRect.height/2+fixedRect.height/2) {
@@ -25,17 +27,9 @@ function draw() {
   else{
     fixedRect.shapeColor = "blue";
     movingRect.shapeColor = "blue";
-  }
-
-  /*if(movingRect.y - fixedRect.y < movingRect.height/2+fixedRect.height/2 && 
-    fixedRect.y - movingRect.y < movingRect.height/2+fixedRect.height/2) {
-    fixedRect.shapeColor = "red";
-    movingRect.shapeColor = "red";
-  }
-  else{
-    fixedRect.shapeColor = "blue";
-    movingRect.shapeColor = "blue";
   }*/
-  
+
+  bounceOff(movingRect, fixedRect);
   drawSprites();
 }
+
